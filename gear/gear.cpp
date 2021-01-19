b363a0cb7b8d08c0a0b91a4591bdc23a3fa851de
Marc-André St-Jean <ma@stj-ma.com>
2021-01-18 21:16:10 -0500

    Gear ratio completed

--- gear/gear.cpp
+++ gear/gear.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
@@ -9,19 +10,88 @@ int main()
 
 
    // Gear ratio = chainring / cog
-	int chainring;
-	int cog;
+	int chainringMin = 40;
+	int chainringMax = 50;
+	
+	int cogMin = 10;
+	int coMax = 20;
 
-	cout << "Chainring";
-	cin >> chainring;
+	
 
-	cout << "cog";
-	cin >> cog;
+	/*
+	cout << "Chainring min";
+	cin >> chainringMin;
 
+	cout << "Chainring Max";
+	cin >> chainringMax;
 
+	cout << "cog Min";
+	cin >> cogMin;
+
+	cout << "cog max";
+	cin >> coMax;
+	*/
+
+	int chainringCount = ((chainringMax - chainringMin) + 1);
+	int cogCount = ((cogMin - coMax) + 1);
 
 
 	
+	//All chainring number
+	vector<int> chainringArrays;
+	for (int i = chainringMin; i <= chainringMax; i++)
+	{
+		//cout << i << " ";
+		chainringArrays.push_back(i);
+	}
+	//for (int i = 0; i < (int)chainringArrays.size(); i++)
+		//cout << chainringArrays.at(i) << endl;
+
+	//All cog numbers
+	vector<int> cogArrays;
+	for (int i = cogMin; i <= coMax; i++)
+	{
+		cogArrays.push_back(i);
+		//cout << i << " ";
+	}
+
+	
+	
+	
+
+	//for (int i = 0; i < (int)cogArrays.size(); i++)
+		//cout << cogArrays.at(i) << endl;
+	
+	
+	vector<vector<double>> gearRatioResult;
+	for (int i = 0; i < cogArrays.size(); i++)
+	{
+		
+		vector<double> gearRatioPartial;
+		for (int ii = 0; ii < chainringArrays.size(); ii++)
+		{
+			double ratio = (double)chainringArrays[ii] / (double)cogArrays[i];
+			//cout << ratio << "\n";
+			gearRatioPartial.push_back(ratio);
+		}
+		gearRatioResult.push_back(gearRatioPartial);
+	}
+
+
+
+	for (int i = 0; i < cogArrays.size(); i++)
+	{
+
+		
+		for (int ii = 0; ii < chainringArrays.size(); ii++)
+		{
+			//cout << "ii " << (double)chainringArrays[ii] << " " << "i " << cogArrays[i] << "\n";
+			cout << gearRatioResult[i][ii] << "\n";
+		}
+		
+	}
+	
+
    // Inch = Ratio / Wheel diameter (Inch)
 
    // meters =  ratio * wheel circumference(meters)
